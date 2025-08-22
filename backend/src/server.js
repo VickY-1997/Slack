13d67f0d7f6a1629abb5aadf5cbdabb9aceeecda
VickY-1997 <vigneshkumar3497@gmail.com>
2025-08-22 17:25:49 +0530

    new clerk setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,14 +2,35 @@ import express from "express";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "./config/inngest.js";
 
 const app = express();
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/", (req, res) => {
   res.send("Hi");
 });
 
-app.listen(ENV.PORT, () => {
-  connectDB();
-  console.log(`The server is running on port ${ENV.PORT}`);
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    if (ENV.NODE_ENV !== "production") {
+      app.listen(ENV.PORT, () => {
+        console.log(`The server is running on port ${ENV.PORT}`);
+      });
+    }
+  } catch (error) {
+    console.log(`Error in starting server : ${error.message}`);
+    process.exit(1);
+  }
+};
+
+startServer();
+
+export default app;
